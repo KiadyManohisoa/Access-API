1b9e0fccfe6725311b59c72624001a1a8309ba20
Kiady <kiadyandriamanantsilavo@gmail.com>
2024-12-18 15:11:45 +0300

    initialisation

---
+++ script/db/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO Genre (libelle) VALUES ('homme'), ('femme');
+
+INSERT INTO Utilisateur (mail, mdp, nom, prenom, date_naissance, idGenre) VALUES 
+('jeanaime@gmail.com', 'aeae12jj', 'Rasolo', 'Jean Aimé', '2004-11-18', 'GR01');
+
+INSERT INTO Compte (d_date_debloquage, d_pin_actuel, d_date_expiration_pin, id_Utilisateur) 
+VALUES (null, null, null, 'USR000000002');

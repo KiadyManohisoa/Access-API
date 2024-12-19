b8333d3df7c6a003c987276a40befa4024e8ce68
Manampiniaina <155744150+Manampiniaina@users.noreply.github.com>
2024-12-19 09:10:37 +0300

    get_swagger

--- API project/inscription/install-dependance.bat
+++ API project/inscription/install-dependance.bat
@@ -2,3 +2,4 @@ composer require symfony/framework-bundle
 composer require symfony/http-foundation
 composer require doctrine/orm
 composer require symfony/orm-pack
+composer require zircote/swagger-php
